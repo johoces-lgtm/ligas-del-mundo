5b686135b71f08bba55c234e5b219e041c55d9f0
johoces <jo.hoces@duocuc.cl>
2026-06-18 10:50:28 -0400

    fix: limpieza estructural y subida exclusiva de paises

--- ligas-del-mundo-alonso-servicio-estadios/ligas-del-mundo-alonso-servicio-estadios/README.md
+++
@@ -1,1 +0,0 @@
-# ligas-del-mundo
--- ligas-del-mundo-alonso-servicio-estadios/ligas-del-mundo-alonso-servicio-estadios/estadios/estadios/src/main/java/duoc/cl/estadios/repository/EstadioRepository.java
+++
@@ -1,9 +0,0 @@
-package duoc.cl.estadios.repository;
-
-import duoc.cl.estadios.model.EstadioModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstadioRepository extends JpaRepository<EstadioModel, Long> {
-}
