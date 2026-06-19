d7630d2657ed17fb96bed4283bcb20c607abc858
johoces-lgtm <jo.hoces@duocuc.cl>
2026-06-19 18:59:28 -0400

    Delete src directory

--- src/main/java/cl/duoc/ligas/repository/LigasRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.duoc.ligas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cl.duoc.ligas.model.LigasModel;
-
-public interface LigasRepository  extends JpaRepository<LigasModel, Long> {
-
-}
