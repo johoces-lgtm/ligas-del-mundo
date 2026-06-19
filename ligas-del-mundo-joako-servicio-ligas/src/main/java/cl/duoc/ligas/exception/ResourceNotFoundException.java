55d850559ff48eecbbcf3b7cbec17c5f8e78df41
johoces-lgtm <jo.hoces@duocuc.cl>
2026-06-19 19:42:02 -0400

    Delete ligas-del-mundo-joako-servicio-ligas directory

--- ligas-del-mundo-joako-servicio-ligas/src/main/java/cl/duoc/ligas/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.ligas.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
