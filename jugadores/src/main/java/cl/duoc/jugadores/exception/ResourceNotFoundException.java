8a7129096315a12c32753eef83e67d3952e10220
johoces-lgtm <jo.hoces@duocuc.cl>
2026-06-19 20:23:55 -0400

    Delete jugadores directory

--- jugadores/src/main/java/cl/duoc/jugadores/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.jugadores.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
