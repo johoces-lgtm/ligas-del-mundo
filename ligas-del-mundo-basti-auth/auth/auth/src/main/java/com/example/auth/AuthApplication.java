6d17260d9cff321991e18e58c30cede0769b3617
bvasquezs <bvasquezs@duocuc.cl>
2026-06-21 16:14:50 -0400

    se ve el tema del docker

--- ligas-del-mundo-basti-auth/auth/auth/src/main/java/com/example/auth/AuthApplication.java
+++ ligas-del-mundo-basti-auth/auth/auth/src/main/java/com/example/auth/AuthApplication.java
@@ -2,13 +2,11 @@ package com.example.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(excludeName = {
     "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
     "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
 })
-@EnableDiscoveryClient
 public class AuthApplication {
 
     public static void main(String[] args) {
